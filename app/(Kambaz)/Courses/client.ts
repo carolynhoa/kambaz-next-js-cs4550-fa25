6af916863ebf7eadd7b55833ac2bf76878f3b2e7
Carolyn Hoa <hoacarolyn@gmail.com>
2025-12-02 00:21:20 -0500

    debug axios error

--- app/(Kambaz)/Courses/client.ts
+++ app/(Kambaz)/Courses/client.ts
@@ -82,7 +82,7 @@ export const deleteModule = async (courseId: string, moduleId: string) => {
 };
 
 export const findUsersForCourse = async (courseId: string) => {
-  const response = await axios.get(`${COURSES_API}/${courseId}/users`);
+  const response = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/users`);
   return response.data;
  };
 
